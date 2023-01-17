8feb170ef138dedd28156d1243e7f7c6fcef0e29
Roberto Capotosti <online@robertocapotosti.it>
2023-01-17 23:42:57 +0000

    commit 1

--- semantic.py
+++ semantic.py
@@ -2,3 +2,8 @@
 
 import spacy
 nlp = spacy.load('en_core_web_md')
+
+tokens = nlp('cat apple monkey banana ')
+for token1 in tokens:
+	for token2 in tokens:
+		print(token1.text, token2.text, token1.similarity(token2))
